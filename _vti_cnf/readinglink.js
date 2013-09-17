f330665827a55c5cb3af28b2895744b2a0843b82
mtigue <mtigue@web4uonline.com>
2013-09-17 18:28:15 -0400

    1

---
+++ _vti_cnf/readinglink.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|16 Sep 2013 20:17:13 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Toshiba\\mtigue
+vti_modifiedby:SR|Toshiba\\mtigue
+vti_timecreated:TR|16 Sep 2013 20:17:13 -0000
+vti_cacheddtm:TX|16 Sep 2013 20:17:13 -0000
+vti_filesize:IR|878
+vti_backlinkinfo:VX|31/main-sep16.html 31/main_copy(1).html 31/main.html
